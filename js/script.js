ed11860bd288e138b8e2fc1a4dba5ffdc9884f23
j-deamon <82471286+j-daemon@users.noreply.github.com>
2024-07-25 21:32:36 -0400

    add makeGrid function to js and set flex-wrap

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 console.log("Welcome to Etch-a-Sketch!")
+
+// seleect the container div
+
+let container = document.querySelector(".container");
+
+// function to append ?? x ?? grid squares
+
+function makeGrid(num1, num2) {
+    for (let i = 0; i < (num1 * num2); i++) {
+
+        // create box element
+
+        let box = document.createElement("div");
+        box.setAttribute("id", "grid-box");
+        
+        console.log(i)
+
+        // append box element to container
+        
+        container.appendChild(box);
+    }
+}
+
+// create grid
+
+makeGrid(16, 16)
+
